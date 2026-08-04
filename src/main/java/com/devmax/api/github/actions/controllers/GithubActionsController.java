1244452824a8197cec75011b512676bcb6e893e0
luis.carranza.ext <luis.carranza.ext@gmail.com>
2026-08-04 17:16:13 -0500

    Mensaje cambiado

--- src/main/java/com/devmax/api/github/actions/controllers/GithubActionsController.java
+++ src/main/java/com/devmax/api/github/actions/controllers/GithubActionsController.java
@@ -9,6 +9,6 @@ public class GithubActionsController {
 
     @RequestMapping
     public String actions(){
-        return "Github Actions Linux";
+        return "Github Actions Linux-Windows";
     }
 }
